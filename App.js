2f1a9d8cb57b785d8b09b4ea989ffcaf65dc6129
Chipskein <abfn0905@gmail.com>
2022-11-12 12:00:08 -0300

    so vai

--- App.js
+++ App.js
@@ -10,139 +10,75 @@ import {
 } from "react-native";
 import Constants from "expo-constants";
 import * as SQLite from "expo-sqlite";
+import { Pull } from './pages/api'
 
-function openDatabase() {
-  if (Platform.OS === "web") {
-    return {
-      transaction: () => {
-        return {
-          executeSql: () => {},
-        };
-      },
-    };
-  }
-
-  const db = SQLite.openDatabase("test-database.sqlite");
-  return db;
-}
-
-const db = openDatabase();
-
-function Items({onPressItem}) {
-  const [items, setItems] = useState(null);
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "select * from users2",[],
-        (_, { rows: { _array } }) => setItems(_array)
-        );
-      });
-    }, []);
-    console.log(items)
-
-  return (
-    <View style={styles.sectionContainer}>
-      {items && items.map(({ id, name }) => (
-        <TouchableOpacity
-          key={id}
-          onPress={() => onPressItem && onPressItem(id)}
-          style={{
-            borderColor: "#000",
-            borderWidth: 1,
-            padding: 8,
-          }}
-        >
-          <Text>{name}</Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-}
 
 export default function App() {
   const [text, setText] = useState(null);
+  const [items, setItems] = useState(null);
   const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
   const [senha, setSenha] = useState(null);
-  const [forceUpdate, forceUpdateId] = useForceUpdate();
+  const [db,setDb] =useState(null)
+  console.log(items)
+  async function getDatabase() {
+    const uri_Database=await Pull()
+    if(uri_Database){
+      const db = SQLite.openDatabase(uri_Database);
+      setDb(db)
+      return db;
+    }
+  }
+  function Items({onPressItem}) {
+    return (
+      <View style={styles.sectionContainer}>
+        {items && items.map(({ id, name }) => (
+          <TouchableOpacity
+            key={id}
+            onPress={() => onPressItem && onPressItem(id)}
+            style={{
+              borderColor: "#000",
+              borderWidth: 1,
+              padding: 8,
+            }}
+          >
+            <Text>{name}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    );
+  }
 
   useEffect(() => {
+     getDatabase()
+  }, []);
+
+
+  useEffect(() => {
+    if(!db) return;
     db.transaction((tx) => {
-      tx.executeSql(
-        "create table if not exists users2 (id integer primary key not null, name text, email text, password text);"
+    tx.executeSql(
+      "select * from users",[],
+      (_, { rows: { _array } }) => setItems(_array)
       );
     });
-  }, []);
+  }, [db,items]);
   const add = (name,email,senha) => {
-    // is text empty?
-
+    if(!db) return
     db.transaction(
       (tx) => {
-        tx.executeSql("insert into users2 (name,email,password) values (?,?,?)", [name,email,senha]);
-        tx.executeSql("select * from users2", [], (_, { rows }) =>
-          console.log('rows',JSON.stringify(rows))
-        );
+        tx.executeSql("insert into users (name,email,password) values (?,?,?)", [name,email,senha]);
+        tx.executeSql(
+          "select * from users", [], 
+          (_, { rows: { _array } }) => setItems(_array)
+        )
       },
-      null,
-      forceUpdate
+      null
     );
   };
 
   return (
     <View style={styles.container}>
-
-      {/* {Platform.OS === "web" ? (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <Text style={styles.heading}>
-            Expo SQlite is not supported on web!
-          </Text>
-        </View>
-      ) : (
-        <>
-          <View style={styles.flexRow}>
-            <TextInput
-              onChangeText={(text) => setText(text)}
-              onSubmitEditing={() => {
-                add(text);
-                setText(null);
-              }}
-              placeholder="what do you need to do?"
-              style={styles.input}
-              value={text}
-            />
-          </View>
-          <ScrollView style={styles.listArea}>
-            <Items
-              key={`forceupdate-todo-${forceUpdateId}`}
-              onPressItem={(id) =>
-                db.transaction(
-                  (tx) => {
-                    tx.executeSql(`update users2 set name = 'opa' where id = ?;`, [
-                      id,
-                    ]);
-                  },
-                  null,
-                  forceUpdate
-                )
-              }
-            />
-            <Items
-              key={`forceupdate-done-${forceUpdateId}`}
-              onPressItem={(id) =>
-                db.transaction(
-                  (tx) => {
-                    tx.executeSql(`delete from users2 where id = ?;`, [id]);
-                  },
-                  null,
-                  forceUpdate
-                )
-              }
-            />
-          </ScrollView>
-        </>
-      )} */}
       <TextInput
         onChangeText={(text) => setName(text)}
         placeholder="name"
@@ -168,28 +104,46 @@ export default function App() {
         <TouchableOpacity onPress={()=>{add(name,email,senha);}}><Text>Salvar</Text></TouchableOpacity>
         <ScrollView style={styles.listArea}>
             <Items
-              key={`forceupdate-todo-${forceUpdateId}`}
-              onPressItem={(id) =>
-                db.transaction(
-                  (tx) => {
-                    tx.executeSql(`update users2 set name = 'opa' where id = ?;`, [
-                      id,
-                    ]);
-                  },
-                  null,
-                  forceUpdate
-                )
+            key={Math.random()*3}
+              onPressItem={(id) =>{
+                  //if(!db) return;
+                  db.transaction(
+                    (tx) => {
+                      tx.executeSql(`update users set name = 'vsf' where id = ?`, [
+                        id,
+                      ]);
+                      tx.executeSql("select * from users", [], 
+                      (_, { rows: { _array } }) => setItems(_array))
+                    },
+                    null
+                  )
+                }
               }
             />
           </ScrollView>
+          <TextInput
+        onChangeText={(text) => setName(text)}
+        placeholder="mudar nome"
+        style={styles.input}
+        value={name}/>  
+        <TouchableOpacity onPress={()=>{
+          db.transaction(
+            (tx) => {
+              tx.executeSql(`update users set name = ? where id = ?`, [name,1,
+              ]);
+              tx.executeSql("select * from users", [], 
+              (_, { rows: { _array } }) => setItems(_array))
+            },
+            null
+          )
+        }}>
+          <Text>Salvar</Text>
+
+        </TouchableOpacity>
     </View>
   );
 }
 
-function useForceUpdate() {
-  const [value, setValue] = useState(0);
-  return [() => setValue(value + 1), value];
-}
 
 const styles = StyleSheet.create({
   container: {
---
+++ pages/api.js
@@ -0,0 +1,15 @@
+import * as FileSystem from 'expo-file-system'
+const BASE_API = 'http://localhost:3001';
+const DEFAULT_DATABASE_PATH=`${FileSystem.documentDirectory}/SQLite/`
+export async function Pull() {
+    try{
+        // if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'db.sqlite')).exists) {
+        //     await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'db.sqlite');
+        //   }
+        //await FileSystem.downloadAsync(`${BASE_API}/sync`,DEFAULT_DATABASE_PATH + 'db.sqlite')
+        return 'db.sqlite'
+    }
+    catch(err){
+        console.log(err)
+    }
+}
